21b9b1834d6d20c7b971f7087e843ed89984cbe8
sai chandu <saic9060@gmail.com>
2025-04-03 22:36:12 +0530

    h

--- backend/app.py
+++ backend/app.py
@@ -93,5 +93,5 @@ def predict():
         return jsonify({"error": f"An error occurred: {e}"}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
